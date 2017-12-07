23363bef3683233d1ee4a2ad41cb5c557b6c5b5d
GeforceLee <314004856@qq.com>
2017-12-07 15:55:31 +0800

    init

---
+++ src/main/java/com/eget/admin/EgetAdminApplication.java
@@ -0,0 +1,12 @@
+package com.eget.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgetAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EgetAdminApplication.class, args);
+	}
+}
